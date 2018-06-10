abb84f4b800d48183659647429d3bbf1b6279b41
Maxim Salikhov <msalihov@gmail.com>
2018-06-10 13:36:59 -0400

    Add meta tags and update CSS/JS

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
-var typerValue = 'Maxim';
-var typerIndex = 0;
-var typeDelay = 300;
+const typerValue = 'Maxim';
+let typerIndex = 0;
+const typeDelay = 300;
 
-var typeInterval = setInterval(typeChar, typeDelay);
+const typeInterval = setInterval(typeChar, typeDelay);
 
 function typeChar() {
-    if (typerIndex < typerValue.length) {
-        var typerElement = document.getElementById('typer');
-        var existingHTML = typerElement.innerHTML;
-        typerElement.innerHTML = existingHTML + typerValue.charAt(typerIndex);
-        typerIndex++;
-    } else {
-        clearInterval(typeInterval);
-    }
+  if (typerIndex < typerValue.length) {
+    var typerElement = document.getElementById('typer');
+    var existingHTML = typerElement.innerHTML;
+    typerElement.innerHTML = existingHTML + typerValue.charAt(typerIndex);
+    typerIndex++;
+  } else {
+    clearInterval(typeInterval);
+  }
 }
